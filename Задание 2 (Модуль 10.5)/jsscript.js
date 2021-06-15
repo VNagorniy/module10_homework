b51b88e17730e18910c66da46ec66dbb5a28e662
VNagorniy <78801932+VNagorniy@users.noreply.github.com>
2021-06-15 23:26:57 +0300

    Delete Задание 2 (Модуль 10.5) directory

--- Задание 2 (Модуль 10.5)/jsscript.js
+++
@@ -1,9 +0,0 @@
-/* Задание 2
-Сверстайте кнопку, клик на которую будет выводить данные о размерах экрана с помощью alert.
-*/
-
-const btn = document.querySelector('.btn');
-
-btn.addEventListener('click', () => {
-  alert(`Высота вашего экрана (с учётом полосы прокрутки) ${window.innerHeight}, а Ширина ${window.innerWidth}`)
-})
